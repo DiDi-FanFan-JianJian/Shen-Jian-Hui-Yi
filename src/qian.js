e124243c00b39c3dae9bebc1b8f9e79e24b26935
zhao-fw <74136972+zhao-fw@users.noreply.github.com>
2022-06-14 22:03:54 +0800

    简单网页

---
+++ src/qian.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/login', (req, res) => {
+    console.log(req.body);
+    res.setHeader('Content-Type', 'text/html;charset=gbk');
+    res.sendFile(`${__dirname}/login.html`);
+});
+
+app.get('/index', (req, res) => {
+    console.log(req.body);
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/personal', (req, res) => {
+    console.log(req.body);
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(`${__dirname}/personal.html`);
+});
+
+app.listen(3001, () => {
+    console.log('server running at http://43.142.102.170:3001');
+})
