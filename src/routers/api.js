4824f56b9c75fa6c1f8e47b66e15efd0e0e62b52
DinoMax00 <527663226@qq.com>
2022-06-16 00:39:43 +0800

    缩进

--- src/routers/api.js
+++ src/routers/api.js
@@ -8,17 +8,17 @@ router.get('/getAllStu', (req, res) => {
   console.log(req.body);
 
   var connection = mysql.createConnection({
-      host: '43.142.102.170',
-      user: 'root',
-      password: 'ZhaoLinux2022',
-      database: 'user'
+    host: '43.142.102.170',
+    user: 'root',
+    password: 'ZhaoLinux2022',
+    database: 'user'
   });
   connection.connect();
   var sql = "select stu_no, stu_name from student where stu_userlevel=0";
   connection.query(sql, function (error, results) {
-      if (error) throw console.error;
-      res.json(results)
-      console.log(results)
+    if (error) throw console.error;
+    res.json(results)
+    console.log(results)
   })
   connection.end();
 });
@@ -29,23 +29,23 @@ router.get('/getRole', (req, res) => {
   var stu_no = req.query.stu_no;
 
   var connection = mysql.createConnection({
-      host: '43.142.102.170',
-      user: 'root',
-      password: 'ZhaoLinux2022',
-      database: 'user'
+    host: '43.142.102.170',
+    user: 'root',
+    password: 'ZhaoLinux2022',
+    database: 'user'
   });
   connection.connect();
 
   var sql = "select stu_userlevel from student where stu_no=?";
   connection.query(sql, [stu_no], function (error, results) {
-      if (error) throw console.error;
-      let userlevel = results[0].stu_userlevel;
-      if (userlevel == '1') {
-          res.json({role: "teacher"});
-      }
-      else {
-          res.json({role: "student"});
-      }
+    if (error) throw console.error;
+    let userlevel = results[0].stu_userlevel;
+    if (userlevel == '1') {
+      res.json({ role: "teacher" });
+    }
+    else {
+      res.json({ role: "student" });
+    }
   })
 
   connection.end();
@@ -59,40 +59,40 @@ router.post('/getLogin', (req, res) => {
   console.log(req.body);
 
   var connection = mysql.createConnection({
-      host: '43.142.102.170',
-      user: 'root',
-      password: 'ZhaoLinux2022',
-      database: 'user'
+    host: '43.142.102.170',
+    user: 'root',
+    password: 'ZhaoLinux2022',
+    database: 'user'
   });
   connection.connect();
   var sql = "select stu_password, stu_userlevel, stu_enable from student where stu_no=?";
   connection.query(sql, [stu_no], function (error, results) {
-      if (error) throw console.error;
-      console.log(results);
-      if (results.length == 0) {
-          res.json({msg: "Can't find your account!!!", canLogin: false, role: '?', firstLogin: false});
-      }
-      else if (results.length > 1) {
-          res.json({msg: "System Error!!!", canLogin: false, role: '?', firstLogin: false});
-      }
-      else if (results[0].stu_password != stu_password) {
-          res.json({msg: "Password error!!!", canLogin: false, role: '?', firstLogin: false});
-      }
-      else if (results[0].stu_enable != '1') {
-          res.json({msg: "Your account has been blocked!!!", canLogin: false, role: '?', firstLogin: false});
+    if (error) throw console.error;
+    console.log(results);
+    if (results.length == 0) {
+      res.json({ msg: "Can't find your account!!!", canLogin: false, role: '?', firstLogin: false });
+    }
+    else if (results.length > 1) {
+      res.json({ msg: "System Error!!!", canLogin: false, role: '?', firstLogin: false });
+    }
+    else if (results[0].stu_password != stu_password) {
+      res.json({ msg: "Password error!!!", canLogin: false, role: '?', firstLogin: false });
+    }
+    else if (results[0].stu_enable != '1') {
+      res.json({ msg: "Your account has been blocked!!!", canLogin: false, role: '?', firstLogin: false });
+    }
+    else {
+      if (results[0].stu_userlevel == 1) {
+        res.json({ msg: "success", canLogin: true, role: 'teacher', firstLogin: false })
       }
       else {
-          if (results[0].stu_userlevel == 1) {
-              res.json({msg: "success", canLogin: true, role: 'teacher', firstLogin: false})
-          }
-          else {
-              if (results[0].stu_password == md5(stu_no)) {
-                  res.json({msg: "success", canLogin: true, role: 'student', firstLogin: true});
-              } else {
-                  res.json({msg: "success", canLogin: true, role: 'student', firstLogin: false})
-              }    
-          }
+        if (results[0].stu_password == md5(stu_no)) {
+          res.json({ msg: "success", canLogin: true, role: 'student', firstLogin: true });
+        } else {
+          res.json({ msg: "success", canLogin: true, role: 'student', firstLogin: false })
+        }
       }
+    }
   })
 
   connection.end();
@@ -107,34 +107,34 @@ router.post('/changePassword', (req, res) => {
   console.log(req.body);
 
   var connection = mysql.createConnection({
-      host: '43.142.102.170',
-      user: 'root',
-      password: 'ZhaoLinux2022',
-      database: 'user'
+    host: '43.142.102.170',
+    user: 'root',
+    password: 'ZhaoLinux2022',
+    database: 'user'
   });
   connection.connect();
   var sql;
   // 先查询密码是否正确
   sql = "select COUNT(*) as num from student where stu_no=? and stu_password=?";
-  connection.query(sql, [stu_no, oldpassword], function(error, results) {
-      if (error) throw console.error;
-      console.log(results[0]);
-      if (results[0].num == 1) {
-          // 密码正确则进行更新
-          sql = "update student set stu_password=? where stu_no=?";
-          connection.query(sql, [newpassword, stu_no], function (error, results) {
-              if (error) console.log(error);
-              console.log(results);
-              res.json({msg: 'success'});
-              connection.end();
-          })
-      }
-      else if (results[0].num == 0) {
-          res.json({msg: 'password error'});
-      }
-      else {
-          res.json({msg: 'system error'});
-      }
+  connection.query(sql, [stu_no, oldpassword], function (error, results) {
+    if (error) throw console.error;
+    console.log(results[0]);
+    if (results[0].num == 1) {
+      // 密码正确则进行更新
+      sql = "update student set stu_password=? where stu_no=?";
+      connection.query(sql, [newpassword, stu_no], function (error, results) {
+        if (error) console.log(error);
+        console.log(results);
+        res.json({ msg: 'success' });
+        connection.end();
+      })
+    }
+    else if (results[0].num == 0) {
+      res.json({ msg: 'password error' });
+    }
+    else {
+      res.json({ msg: 'system error' });
+    }
   })
 });
 module.exports = router;
