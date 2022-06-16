ad6ad25a7ef42e642245b1dc22376e2d3b9b5421
zhao-fw <74136972+zhao-fw@users.noreply.github.com>
2022-06-16 20:43:40 +0800

    修改session

--- src/routers/api.js
+++ src/routers/api.js
@@ -5,12 +5,13 @@ const mysql = require('mysql')
 
 // 用来获取所有的学生信息（学号和姓名）
 router.get('/getAllStu', (req, res) => {
-  console.log(req.body);
+  console.log("getAllStu");
+  console.log(req.session);
 
   var connection = mysql.createConnection({
     host: '43.142.102.170',
     user: 'root',
-    password: 'ZhaoLinux2022',
+    password: 'Sj666',
     database: 'user'
   });
   connection.connect();
@@ -18,24 +19,23 @@ router.get('/getAllStu', (req, res) => {
   connection.query(sql, function (error, results) {
     if (error) throw console.error;
     res.json(results)
-    console.log(results)
   })
   connection.end();
 });
 
 // 获取当前学号对应的用户身份（teacher or student）
 router.get('/getRole', (req, res) => {
-  console.log(req.body);
-  var stu_no = req.query.stu_no;
+  console.log("getRole");
+  console.log(req.session);
 
+  var stu_no = req.query.stu_no;
   var connection = mysql.createConnection({
     host: '43.142.102.170',
     user: 'root',
-    password: 'ZhaoLinux2022',
+    password: 'Sj666',
     database: 'user'
   });
   connection.connect();
-
   var sql = "select stu_userlevel from student where stu_no=?";
   connection.query(sql, [stu_no], function (error, results) {
     if (error) throw console.error;
@@ -53,42 +53,69 @@ router.get('/getRole', (req, res) => {
 
 // 登录的post请求，判断用户名是否存在，用户名密码是否匹配，是否被封禁，是否首次登录
 router.post('/getLogin', (req, res) => {
+  console.log("getLogin");
+  console.log(req.session);
+
   const stu_no = req.body.stu_no;
   const stu_password = md5(req.body.stu_password);
-
-  console.log(req.body);
-
   var connection = mysql.createConnection({
     host: '43.142.102.170',
     user: 'root',
-    password: 'ZhaoLinux2022',
+    password: 'Sj666',
     database: 'user'
   });
   connection.connect();
   var sql = "select stu_password, stu_userlevel, stu_enable from student where stu_no=?";
   connection.query(sql, [stu_no], function (error, results) {
     if (error) throw console.error;
-    console.log(results);
     if (results.length == 0) {
+      req.session.username = null;
+      req.session.role = null;
+      req.session.isLogin = 0;
+      req.session.firstLogin = 0;
       res.json({ msg: "Can't find your account!!!", canLogin: false, role: '?', firstLogin: false });
     }
     else if (results.length > 1) {
+      req.session.username = null;
+      req.session.role = null;
+      req.session.isLogin = 0;
+      req.session.firstLogin = 0;
       res.json({ msg: "System Error!!!", canLogin: false, role: '?', firstLogin: false });
     }
     else if (results[0].stu_password != stu_password) {
+      req.session.username = null;
+      req.session.role = null;
+      req.session.isLogin = 0;
+      req.session.firstLogin = 0;
       res.json({ msg: "Password error!!!", canLogin: false, role: '?', firstLogin: false });
     }
     else if (results[0].stu_enable != '1') {
+      req.session.username = null;
+      req.session.role = null;
+      req.session.isLogin = 0;
+      req.session.firstLogin = 0;
       res.json({ msg: "Your account has been blocked!!!", canLogin: false, role: '?', firstLogin: false });
     }
     else {
       if (results[0].stu_userlevel == 1) {
+        req.session.username = req.body.stu_no;
+        req.session.role = "teacher";
+        req.session.isLogin = 1;
+        req.session.firstLogin = 0;
         res.json({ msg: "success", canLogin: true, role: 'teacher', firstLogin: false })
       }
       else {
         if (results[0].stu_password == md5(stu_no)) {
+          req.session.username = req.body.stu_no;
+          req.session.role = "student";
+          req.session.isLogin = 1;
+          req.session.firstLogin = 1;
           res.json({ msg: "success", canLogin: true, role: 'student', firstLogin: true });
         } else {
+          req.session.username = req.body.stu_no;
+          req.session.role = "student";
+          req.session.isLogin = 1;
+          req.session.firstLogin = 0;
           res.json({ msg: "success", canLogin: true, role: 'student', firstLogin: false })
         }
       }
@@ -98,18 +125,30 @@ router.post('/getLogin', (req, res) => {
   connection.end();
 });
 
-// 修改密码
+// 退出登录, 删除session
+router.get('/getLogout', function (req, res) {
+  console.log('getLogout');
+  console.log(req.session);
+
+  req.session.username = null;
+  req.session.role = null;
+  req.session.isLogin = 0;
+  req.session.firstLogin = 0;
+  res.redirect('login');
+});
+
+// 修改密码, 如果是第一次修改则同时修改session
 router.post('/changePassword', (req, res) => {
+  console.log("changePassword");
+  console.log(req.session);
+  
   const stu_no = req.body.stu_no;
   const oldpassword = md5(req.body.oldpassword);
   const newpassword = md5(req.body.newpassword);
-
-  console.log(req.body);
-
   var connection = mysql.createConnection({
     host: '43.142.102.170',
     user: 'root',
-    password: 'ZhaoLinux2022',
+    password: 'Sj666',
     database: 'user'
   });
   connection.connect();
@@ -118,13 +157,14 @@ router.post('/changePassword', (req, res) => {
   sql = "select COUNT(*) as num from student where stu_no=? and stu_password=?";
   connection.query(sql, [stu_no, oldpassword], function (error, results) {
     if (error) throw console.error;
-    console.log(results[0]);
     if (results[0].num == 1) {
       // 密码正确则进行更新
       sql = "update student set stu_password=? where stu_no=?";
       connection.query(sql, [newpassword, stu_no], function (error, results) {
         if (error) console.log(error);
-        console.log(results);
+        if (req.session.firstLogin == 1) {
+          req.session.firstLogin = 0;
+        }
         res.json({ msg: 'success' });
         connection.end();
       })
@@ -137,4 +177,5 @@ router.post('/changePassword', (req, res) => {
     }
   })
 });
+
 module.exports = router;
--- src/routers/index.js
+++ src/routers/index.js
@@ -11,18 +11,48 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  console.log('login');
+  console.log(req.session);
+
   res.setHeader('Content-Type', 'text/html;charset=gbk');
   res.sendFile(path.join(__dirname, views, 'login.html'));
 });
 
 router.get('/index', (req, res) => {
-  res.setHeader('Content-Type', 'text/html;charset=gbk');
-  res.sendFile(path.join(__dirname, views, 'index.html'));
+  console.log("index");
+  console.log(req.session);
+
+  if (req.session.isLogin == 1) {
+    if (req.session.firstLogin == 1) {
+      res.redirect('/password');
+    }
+    else {
+      res.setHeader('Content-Type', 'text/html;charset=gbk');
+      res.sendFile(path.join(__dirname, views, 'index.html'));
+    }
+  }
+  else {
+    res.redirect('/login');
+  }
 });
 
-router.get('/personal', (req, res) => {
-  res.setHeader('Content-Type', 'text/html;charset=gbk');
-  res.sendFile(path.join(__dirname, views, 'personal.html'));
-});
+router.get('/password', (req, res) => {
+  console.log("password");
+  console.log(req.session);
+
+  if (req.session.isLogin == 1) {
+    if (req.session.firstLogin == 1) {
+      res.setHeader('Content-Type', 'text/html;charset=gbk');
+      res.sendFile(path.join(__dirname, views, 'password1.html'));
+    }
+    else {
+      res.setHeader('Content-Type', 'text/html;charset=gbk');
+      res.sendFile(path.join(__dirname, views, 'password2.html'));
+    }
+  }
+  else {
+    res.redirect('/login');
+  }
+})
 
 module.exports = router;
